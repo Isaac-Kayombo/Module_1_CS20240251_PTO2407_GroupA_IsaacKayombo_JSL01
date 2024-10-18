d5d2d6396ba044ff06650d8f0a5cc3f95bdc68ba
IsaacK47 <isaackayombo41@gmail.com>
2024-10-18 19:06:21 +0200

    Doing check 1 logic

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
 function validateSyntax() {
     let input = document.getElementById('petInput').value;
-    // Validation logic goes here
+    
     let result = ''; // Placeholder for validation result
+    
+    // Validation logic goes here
+    // Check if input starts with 'pet_' and followed by alphanumeric characters
 
-    // TODO: Write your validation logic here
-        // Check if input starts with 'pet_' and followed by alphanumeric characters
+    // === CHECK 1 LOGIC === //
 
-            document.getElementById('result').innerText = result;
+    // Extracting first 4 characters form input
+    const part1 = input.slice(0, 4);
+
+    //Initialising boolean for first check
+    let check1 = false;
+
+    // Checking if 4 characters are 'pet_'
+    if (part1 === "pet_") {
+        check1 = true;
+    }
+
+    document.getElementById('result').innerText = result;
 }
 
 
